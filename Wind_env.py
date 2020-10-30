72d85c9d2243fb2e4086c81141ada7c12fbb3343
Fu Yangqing <65768466+FYQ0919@users.noreply.github.com>
2020-10-30 11:40:00 +0800

    Env finished version1.0
    finishi env established

--- Wind_env.py
+++ Wind_env.py
@@ -1,9 +1,14 @@
 import time
 import numpy as np
 import airsim
-import config
 
+#define destination
 object_pos = [200,0,10]
+#define boundary
+outZ = [-5, 20]
+outY = [-10,10]
+
+np.random.seed(10)
 
 class windENV():
 
@@ -12,14 +17,15 @@ class windENV():
         self.cl = airsim.MultirotorClient()
         self.cl.confirmConnection()
         self.action_size = 5
-        self.clock = 1.5
-        self.duration = 0.85/self.clock
+        self.clock = 5
+        self.duration = 0.5/self.clock
 
 
     def reset(self):
         self.cl.reset()
         self.cl.enableApiControl(True)
         self.cl.armDisarm(True)
+        self.add_wind()
 
         #take off
         self.cl.simPause(False)
@@ -42,14 +48,59 @@ class windENV():
             #get states
             pos = self.cl.getMultirotorState().kinematics_estimated.position
             velocity = self.client.getMultirotorState().kinematics_estimated.linear_velocity
+            angle_acc = self.cl.getMultirotorState().kinematics_estimated.angular_acceleration
         self.cl.simPause(True)
-        Img = self.cl.simGetImages([airsim.ImageRequest(1, airsim.ImageType.DepthVis, True)])
 
-        # get states
+
+        # update states
         pos = self.client.getMultirotorState().kinematics_estimated.position
         velocity = self.client.getMultirotorState().kinematics_estimated.linear_velocity
+        angle_acc = self.cl.getMultirotorState().kinematics_estimated.angular_acceleration
+        state = [(pos),(velocity),(angle_acc)]
+        #define stop condition
+        stop = pos.y_val < outY[0] or pos.y_val > outY[1] or pos.z_val < outZ[0] or pos.z_val > outZ[1]
+        bias = pos - object_pos
+        success = np.linalg.norm(bias) < 10
+        done = stop or success
 
-        dead =
+        #compute reward
+        reward = self.compute_reward(pos,velocity,angle_acc,stop)
+
+        angle_acc = np.array([angle_acc.x_val,angle_acc.y_val,angle_acc.z_val])
+        velocity = np.array([velocity.x_val,velocity.y_val,velocity.z_val])
+        observation = [angle_acc,velocity]
+        return  observation, reward, done, state
+
+    def compute_reward(self, pos, velocity, angle_acc,stop):
+        velocity = np.array([velocity.x_val,velocity.y_val,velocity.z_val],dtype=np.float)
+        speed = np.linalg.norm(velocity)
+        angle_acc = np.array([angle_acc.x_val,angle_acc.y_val,angle_acc.z_val],dtype=np.float)
+        weight_mr = 0.5
+        weight_ar = 1.0
+        weight_vr = 0.5
+        if stop:
+            reward = -10
+        else:
+            #define diiferent reward:
+            move_reward =  100./np.linalg.norm(pos-object_pos)
+            speed_reward = speed
+            angle_acc_reward = 100./np.linalg.norm(speed)
+            reward = weight_ar*angle_acc_reward + weight_mr*move_reward + weight_vr*speed_reward
+
+        return reward
+
+    def add_wind(self):
+        w1 = np.random.uniform(0,10,int)
+        w2 = np.random.uniform(0,10,int)
+        w3 = np.random.uniform(0,10,int)
+        wind = airsim.Vector3r(w1,w2,w3)
+        self.cl.simSetWind(wind)
+
+    def disconnect(self):
+        self.cl.enableApiControl(False)
+        self.cl.armDisarm(False)
+        print('Disconnected')
+
 
 
 
